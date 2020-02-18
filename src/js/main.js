063f22cc53e226d44f297499a55eacf7f7f68279
Max Novikov <max.vashuk@gmail.com>
2020-02-18 13:45:15 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import './jquery-global.js';
-import './sliders';
 import './onload-check';
 import './menu-toggle';
 import './inview-check';
--- src/js/onload-check.js
+++ src/js/onload-check.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
   setTimeout(() => {
-    $('.p').addClass('is-loaded');
-  }, 500);
-
-  setTimeout(() => {
     $('main').addClass('is-loaded');
   }, 700);
 });
